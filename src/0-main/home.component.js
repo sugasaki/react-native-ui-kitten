b937eab00a8e8e30d38211f03b3b42ca6b5485c9
sugasaki <sugasaki@gmail.com>
2020-11-01 12:23:11 +0900

    home画面のレイアウト変更

--- src/0-main/home.component.js
+++ src/0-main/home.component.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { Button, Divider, Layout, TopNavigation } from '@ui-kitten/components';
+import { StyleSheet } from 'react-native';
+import { Button, Divider, Layout, TopNavigation, Card, Text } from '@ui-kitten/components';
 
 export const HomeScreen = ({ navigation }) => {
 
@@ -26,21 +27,52 @@ export const HomeScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <TopNavigation title='MyApp HomeScreen' alignment='center'/>
-      <Divider/>
-      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Button onPress={navigateDetails}>ReactNative Origin</Button>
-      <Divider/>
-      <Button onPress={navigateGettingStart}>01 Getting Start</Button>
+
+      <Layout style={styles.topContainer} level='1'>
+
+      <Card style={styles.card} status='success'>
+        <Text style={styles.text} category='h3'>ReactNative初期画面</Text>
+        <Text>ReactNativeの初期画面</Text>
+        <Button onPress={navigateDetails}>ReactNative Origin</Button>
+      </Card>
       <Divider />
-      <Button onPress={navigateBranding01}>02 Branding theme</Button>
+
+      <Card style={styles.card} status='success'>
+        <Text style={styles.text} category='h3'>01 Getting Start </Text>
+        <Text>Getting Startで説明された画面</Text>
+        <Button onPress={navigateGettingStart}>Getting Start</Button>
+      </Card>
       <Divider />
-      <Button onPress={navigateBranding02}>02 Branding backgrounds</Button>
+
+
+      <Card style={styles.card} status='success'>
+        <Text style={styles.text} category='h3'>02 Branding </Text>
+        <Text>UI Kittenは、Eva DesignSystemが提供する2つのビジュアルテーマをサポートしています</Text>
+        <Button onPress={navigateBranding01}>theme</Button>
+        <Divider />
+        <Button onPress={navigateBranding02}>backgrounds</Button>
+        <Divider />
+        <Button onPress={navigateBranding03}>Text and Icons</Button>
+      </Card>
       <Divider />
-      <Button onPress={navigateBranding03}>02 Branding Text and Icons</Button>
-      <Divider />
-      <Button onPress={navigateIconPack}>03 Icon Pack</Button>
-      <Divider />
+      
+      <Card style={styles.card} status='success'>
+        <Text style={styles.text} category='h3'>03 アイコンパッケージ </Text>
+        <Text>Eva Iconsは、一般的なアクションやアイテム用に美しく細工されたオープンソースアイコンのパックです。</Text>
+        <Button onPress={navigateIconPack}>Icon Pack</Button>
+      </Card>
+
       </Layout>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  card: {
+    margin: 2,
+  },
+});
